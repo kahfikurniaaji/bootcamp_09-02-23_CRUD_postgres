938e9f6cc428b284988040744fc09a92ca068816
Kahfi Kurnia Aji <58323098+kahfikurniaaji@users.noreply.github.com>
2023-02-09 11:05:22 +0700

    Tabel contact

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// Information using EJS
+app.set('view engine', 'ejs');
+
 // Request get untuk path root
 app.get('/', (req, res) => {
-    res.sendFile('./index.html', { root: __dirname });
+    // res.sendFile('./index.html', { root: __dirname });
+    res.render('index');
 })
 
 // Request get untuk path /about
@@ -15,7 +19,21 @@ app.get('/about', (req, res) => {
 
 // Request get untuk path /contact
 app.get('/contact', (req, res) => {
-    res.send('this is contact page!');
+    const contacts = [{
+        name: 'Kahfi',
+        mobile: '085721476789'
+    }, {
+        name: 'Kurnia',
+        mobile: '085700000001'
+    }, {
+        name: 'Aji',
+        mobile: '085700000002'
+    }];
+    res.render('contact', {contacts});
+})
+
+app.get('/product/:productId', (req, res) => {
+    res.send(`Product ID : ${req.params.productId}<br>Category Name : ${req.query.category}`);
 })
 
 // Request get untuk path /product/:productId/category/:categoryId
